876aeadba6a1960cd44f6d05c813e71fc0ca3f70
James Segarra III <JamesSegarra@gmail.com>
2016-09-29 15:41:50 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
+  end
 
+  def any_punc_ending
+    self.question? || self.exclamation? || self.sentence?
   end
 
   def count_sentences
-
+    self.split.each_with_object([]) {|piece,obj| obj << piece if piece.any_punc_ending}.length
   end
 end
+
+# test="Three of these. Sentences are?? The Suck!"
+# binding.pry
+# puts test.count_sentences
